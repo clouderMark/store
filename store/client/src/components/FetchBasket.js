e7fffa14bb28bb5af695a9c61ca577923238bdab
clouderMark <charnysheuski@yandex.by>
2023-02-03 10:55:47 +0300

    refactor: do FetchBasket by style rules

--- store/client/src/components/FetchBasket.js
+++ store/client/src/components/FetchBasket.js
@@ -1,27 +1,23 @@
-import { AppContext } from "./AppContext.js"
-import { fetchBasket } from "../http/basketAPI.js"
-import { useContext, useEffect, useState } from "react"
-import { Spinner } from "react-bootstrap"
+import {useContext, useEffect, useState} from 'react';
+import {Spinner} from 'react-bootstrap';
+import {AppContext} from './AppContext.js';
+import {fetchBasket} from '../http/basketAPI.js';
 
 const FetchBasket = (props) => {
-    const { basket } = useContext(AppContext)
-    const [fetching, setFetching] = useState(true)
+  const {basket} = useContext(AppContext);
+  const [fetching, setFetching] = useState(true);
 
-    useEffect(() => {
-        fetchBasket()
-        .then(
-            data => basket.products = data.products
-        )
-        .finally(
-            () => setFetching(false)
-        )
-    }, [])
+  useEffect(() => {
+    fetchBasket() // eslint-disable-next-line
+      .then((data) => (basket.products = data.products))
+      .finally(() => setFetching(false));
+  }, []);
 
-    if (fetching) {
-        return <Spinner animation="border" varian="light" />
-    }
+  if (fetching) {
+    return <Spinner animation="border" varian="light" />;
+  }
 
-    return props.children
-}
+  return props.children;
+};
 
-export default FetchBasket
+export default FetchBasket;
