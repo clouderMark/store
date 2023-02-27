782d63254a0c62982672e5248f57d16ad8d85397
clouderMark <charnysheuski@yandex.by>
2023-02-27 21:08:16 +0300

    feat: add opportunity use inline svg

--- store/client/src/typings.d.ts
+++ store/client/src/typings.d.ts
@@ -4,3 +4,11 @@ declare module '*.css' {
   const classes: {[key: string]: string};
   export default classes;
 }
+
+declare module '*.svg' {
+  import React = require('react');
+
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
