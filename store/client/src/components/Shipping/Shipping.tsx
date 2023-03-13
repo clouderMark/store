34e03edc07639789f16878566612eb37d12d468e
clouderMark <charnysheuski@yandex.by>
2023-03-13 12:14:16 +0300

    refacor: add images to shipping block and refactor styles

--- store/client/src/components/Shipping/Shipping.tsx
+++ store/client/src/components/Shipping/Shipping.tsx
@@ -1,13 +1,19 @@
 import {Box, Typography} from '@mui/material';
 import {box} from './styles/box';
+import img1 from './images/payment-paypal.png';
+import img2 from './images/shipping-dhl.png';
 
 const Shipping = () => (
   <Box sx={box}>
-    <Typography sx={box.content.common} component="h2">
-      <Typography sx={box.content.top} component="span">Виды доставки</Typography>
+    <Typography sx={box.content.title} component="h2">
+      <Typography sx={box.content.top} component="span">
+        Виды доставки
+      </Typography>
       Наши партнеры доставки и оплаты
     </Typography>
-    <Box>
+    <Box sx={box.content.images}>
+      <Box component="img" sx={box.content.img} src={img1} alt="paypal" />
+      <Box component="img" sx={box.content.img} src={img2} alt="dhl"/>
     </Box>
   </Box>
 );
--- store/client/src/components/Shipping/styles/box.ts
+++ store/client/src/components/Shipping/styles/box.ts
@@ -1,20 +1,35 @@
 export const box = {
   minHeight: '569px',
   backgroundColor: '#fbfbfb',
+  padding: '163px 0 124px',
   content: {
     top: {
       fontWeight: 500,
       textTransform: 'uppercase',
       color: '#008f38',
     },
-    common: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      padding: '163px 0 124px',
+    title: {
+      textAlign: 'center',
+      marginBottom: '72px',
       color: '#6f6f6f',
       fontWeight: 300,
       fontSize: '40px',
     },
+    images: {
+      display: 'flex',
+    },
+    img: {
+      width: '153px',
+      height: '135px',
+    },
   },
 };
+
+const common = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+};
+
+Object.assign(box, common);
+Object.assign(box.content.title, common);
