d2551bb4af397cdc964ba7a65bb0147a7f3e0de6
clouderMark <charnysheuski@yandex.by>
2023-03-13 09:21:03 +0300

    refactor: individualProduct block

--- store/client/src/components/IndividualProduct/IndividualProduct.tsx
+++ store/client/src/components/IndividualProduct/IndividualProduct.tsx
@@ -1,59 +1,27 @@
 import {NavLink} from 'react-router-dom';
 import {Box, Typography, Button} from '@mui/material';
-import bg1 from './bg1.png';
-import bg2 from './bg2.png';
+import {box} from './styles/box';
+import {content} from './styles/content';
+import {button} from './styles/button';
 
 const IndividualProduct = () => (
-  <Box
-    sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '281px',
-      backgroundColor: '#008f38',
-      backgroundImage: `url(${bg1}), url(${bg2})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: '0 -150px, right 0',
-      marginTop: '-20px',
-    }}
-  >
+  <Box sx={box}>
     <Typography
       component="h3"
-      sx={{
-        textAlign: 'center',
-        color: 'white',
-        fontSize: '30px',
-        fontWeight: 300,
-      }}
+      sx={content.top}
     >
       Не можете найти нужный продукт?
     </Typography>
     <Typography
       component="p"
-      sx={{
-        textAlign: 'center',
-        color: 'white',
-        fontSize: '18px',
-        fontWeight: 300,
-      }}
+      sx={content.low}
     >
       Мы так же изготавливаем продукты под индивидуальные требования
     </Typography>
     <Button
-      component={NavLink} to='/services'
-      sx={{
-        width: '255px',
-        height: '54px',
-        marginTop: '20px',
-        backgroundColor: 'white',
-        borderRadius: 0,
-        color: '#008f38',
-        '&:hover': {
-          backgroundColor: '#78ae7b',
-          color: 'white',
-        },
-      }}
+      component={NavLink}
+      to="/services"
+      sx={button}
     >
       к разработке продуктов
     </Button>
---
+++ store/client/src/components/IndividualProduct/styles/box.ts
@@ -0,0 +1,15 @@
+import bg1 from '../image/bg1.png';
+import bg2 from '../image/bg2.png';
+
+export const box = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '281px',
+  backgroundColor: '#008f38',
+  backgroundImage: `url(${bg1}), url(${bg2})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: '0 -150px, right 0',
+  marginTop: '-20px',
+};
