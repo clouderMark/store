4d31a64a8a70f3409c18109397aa9e7a26ef6555
clouderMark <charnysheuski@yandex.by>
2023-03-28 12:15:52 +0300

    fix: falling out content from navbar

--- store/client/src/components/NavBar/styles/container.ts
+++ store/client/src/components/NavBar/styles/container.ts
@@ -1,13 +1,19 @@
 import {queryTablet} from '../../../commonContent/queryTablet';
+import {queryMobile} from '../query';
 
 export const container = {
   height: 188,
   pt: '29px',
   mb: '9px',
-  // p: 0,
+
   [`@media (max-width: ${queryTablet}px)`]: {
     height: 105,
     pt: 0.9,
     mb: 0,
+  },
+
+  [`@media (max-width: ${queryMobile}px)`]: {
+    paddingLeft: 0,
+    paddingRight: 0,
   },
 };
