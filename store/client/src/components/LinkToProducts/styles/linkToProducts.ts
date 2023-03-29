ae6ff8e6b94673f41ed675c24f543b256ac4d0f7
clouderMark <charnysheuski@yandex.by>
2023-03-29 13:40:28 +0300

    refactor: this by containerwithcolumns

--- store/client/src/components/LinkToProducts/styles/linkToProducts.ts
+++ store/client/src/components/LinkToProducts/styles/linkToProducts.ts
@@ -1,36 +1,8 @@
-const queryMobile = 732;
-const color = '#6f6f6f';
-
 export const linkToProducts = {
-  container: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    color: color,
-    paddingBottom: '80px',
-
-    [`@media (max-width: ${queryMobile}px)`]: {
-      flexDirection: 'column',
-    },
-  },
-
-  row: {
-    width: '51%',
-
-    [`@media (max-width: ${queryMobile}px)`]: {
-      width: '100%',
-    },
-  },
-
   title: {
-    width: '40%',
     fontSize: '35px',
     fontWeight: 300,
     lineHeight: 1.2,
-
-    [`@media (max-width: ${queryMobile}px)`]: {
-      width: '100%',
-      marginBottom: '16px',
-    },
   },
 
   strong: {
