8f56030f692c054080a7d2dd9aa0988202223ca0
clouderMark <charnysheuski@yandex.by>
2023-03-29 12:08:18 +0300

    feat: add button to shop

--- store/client/src/pages/Main/Main.tsx
+++ store/client/src/pages/Main/Main.tsx
@@ -1,3 +1,5 @@
+import {NavLink} from 'react-router-dom';
+import {Button, Box} from '@mui/material';
 import HeaderImage from '../../components/HeaderImage/HeaderImage';
 import LinkList from '../../components/LinkList/LinkList';
 import LinkToProducts from '../../components/LinkToProducts/LinkToProducts';
@@ -9,6 +11,13 @@ const Main = () => (
     <LinkList />
     <LinkToProducts />
     <Slider />
+    <Box component='p' sx={{width: '100%', display: 'flex', justifyContent: 'center', mt: '70px'}}>
+      <Button component={NavLink}
+        to={'/shop'}
+        variant="contained" color="first">
+        Смотреть все продукты
+      </Button>
+    </Box>
   </>
 );
 
