e1189b574b61a55623a33bdff3362f2ac5d435ff
clouderMark <charnysheuski@yandex.by>
2023-03-30 13:29:22 +0300

    feat: add quality component

---
+++ store/client/src/components/Quality/content.ts
@@ -0,0 +1,24 @@
+import img1 from '../../image/TUeV_Sued_logo-100x100.png';
+import img2 from '../../image/abz.png';
+import img3 from '../../image/hy.png';
+
+export const content = {
+  title: 'Quality',
+  header: 'DIAMANT – Manufacturer with certification',
+  content:
+    'We know what we’re doing. And this has been the case for over 130 years! Through continuous research and development, we now offer the best polymer-based products in terms of function, technology and quality. After the first metal polymer, MM1018, with a general building authority approval, our quality management has been certified according to DIN EN ISO 9001:2015 and ISO 14001.', // eslint-disable-line
+  images: [
+    {
+      img: img1,
+      alt: '',
+    },
+    {
+      img: img2,
+      alt: '',
+    },
+    {
+      img: img3,
+      alt: '',
+    },
+  ],
+};
