3244e7c4510cdc52aaf415b14df7f9e7acea3495
clouderMark <charnysheuski@yandex.by>
2023-03-31 15:45:47 +0300

    feat: add component for image

---
+++ store/client/src/components/CentererImage/CentererImage.tsx
@@ -0,0 +1,22 @@
+import {Box} from '@mui/material';
+
+interface IProps {
+  // eslint-disable-next-line
+  img: any;
+}
+
+const CentererImage = (props: IProps) => (
+  <Box sx={{display: 'flex', justifyContent: 'center'}}>
+    <Box
+      component="img"
+      src={props.img}
+      sx={{
+        width: '100%',
+        maxWidth: '1920px',
+        objectFit: 'cover',
+      }}
+    />
+  </Box>
+);
+
+export default CentererImage;
