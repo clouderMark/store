f11ca10c413f29a233948b3003598b9fd1f97ba6
clouderMark <charnysheuski@yandex.by>
2023-03-31 15:47:24 +0300

    feat: add component for title

---
+++ store/client/src/components/StrongWithTitle/styles/strongWithTitle.ts
@@ -0,0 +1,12 @@
+export const strongWithTitle = {
+  box: {
+    paddingTop: '80px',
+    paddingBottom: '80px',
+  },
+
+  title: {
+    fontSize: '30px',
+    fontWeight: 300,
+    color: '#6f6f6f',
+  },
+};
