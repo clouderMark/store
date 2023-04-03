2f6fc76610e24e625c9dcba7321ad61214792c9a
clouderMark <charnysheuski@yandex.by>
2023-04-03 09:14:36 +0300

    feat: add components for branches view

--- store/client/src/pages/Branches/Branches.tsx
+++ store/client/src/pages/Branches/Branches.tsx
@@ -1,10 +1,27 @@
 import {Container} from '@mui/material';
 import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs';
+import img from './images/branche-header.webp';
+import CentererImage from '../../components/CentererImage/CentererImage';
+import StrongWithTitle from '../../components/StrongWithTitle/StrongWithTitle';
+import BranchesList from '../../components/BranchesList/BranchesList';
+import Newsletter from '../../components/Newsletter/Newsletter';
+import Footer from '../../components/Footer/Footer';
+
+const content = {
+  p: 'Отрасли',
+  title: 'Find the right product for your application',
+};
 
 const Branches = () => (
   <>
+    <CentererImage img={img} />
     <Breadcrumbs />
-    <Container></Container>;
+    <Container maxWidth={false}>
+      <StrongWithTitle content={content} />
+      <BranchesList />
+    </Container>
+    <Newsletter />
+    <Footer />
   </>
 );
 
