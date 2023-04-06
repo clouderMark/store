3219121b54f79c493a3b01a548dd63b12b19b661
clouderMark <charnysheuski@yandex.by>
2023-04-06 15:01:00 +0300

    feat: add container component

--- store/client/src/views/forAll/Branches/BranchesItem.tsx
+++ store/client/src/views/forAll/Branches/BranchesItem.tsx
@@ -1,7 +1,10 @@
 import {useParams} from 'react-router-dom';
+import {Typography} from '@mui/material';
 import {useAppContext} from '../../../components/AppContext';
 import CentererImage from '../../../components/CentererImage/CentererImage';
 import Breadcrumbs from '../../../components/Breadcrumbs/Breadcrumbs';
+import ContainerWithTwoColumns from '../../../components/ContainerWithTwoColumns/ContainerWithTwoColumns';
+import StrongWithTitle from '../../../components/StrongWithTitle/StrongWithTitle';
 
 const BranchesItem = () => {
   const id: number = Number(useParams().id);
@@ -15,6 +18,18 @@ const BranchesItem = () => {
     <>
       <CentererImage img={branch?.headerImage ? process.env.REACT_APP_IMG_URL + branch.headerImage : ''} />
       <Breadcrumbs />
+      <ContainerWithTwoColumns
+        firstColumn={
+          <StrongWithTitle
+            content={{p: currentItem.branchName, title: 'here will be title'}}
+          />
+        }
+        secondColumn={
+          <>
+            <Typography></Typography>
+          </>
+        }
+      />
     </>
   );
 };
