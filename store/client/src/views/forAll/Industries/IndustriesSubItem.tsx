6eb4ed915556acc942eeb82fbb9d04f2418e24f1
clouderMark <charnysheuski@yandex.by>
2023-04-11 14:55:11 +0300

    feat: add container with title and paragraphs

--- store/client/src/views/forAll/Industries/IndustriesSubItem.tsx
+++ store/client/src/views/forAll/Industries/IndustriesSubItem.tsx
@@ -1,7 +1,10 @@
 import {useParams} from 'react-router-dom';
+import {Typography, Box} from '@mui/material';
 import {useAppContext} from '../../../components/AppContext';
 import CentererImage from '../../../components/CentererImage/CentererImage';
 import Breadcrumbs from '../../../components/Breadcrumbs/Breadcrumbs';
+import ContainerWithTwoColumns from '../../../components/ContainerWithTwoColumns/ContainerWithTwoColumns';
+import StrongWithTitle from '../../../components/StrongWithTitle/StrongWithTitle';
 
 const IndustriesSubItem = () => {
   const id: number = Number(useParams().role);
@@ -15,6 +18,25 @@ const IndustriesSubItem = () => {
     <>
       <CentererImage img={item?.headerImage ? process.env.REACT_APP_IMG_URL + item.headerImage : ''} />
       <Breadcrumbs />
+      <ContainerWithTwoColumns
+        firstColumn={
+          <Box sx={{'& div': {pt: 0}}}>
+            <StrongWithTitle
+              content={{p: catalog.subIndustries.find((el) => el.id === id)!.name, title: item?.title!}}
+            />
+          </Box>
+        }
+        secondColumn={
+          <>
+            {item?.paragraphs.map((el) => (
+              <Typography key={el.id} sx={{mb: '10px'}}>
+                {el.value}
+              </Typography>
+            ))}
+            <Typography />
+          </>
+        }
+      />
     </>
   );
 };
