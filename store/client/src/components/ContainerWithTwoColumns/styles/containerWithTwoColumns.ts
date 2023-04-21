a67109c7626ecb9936dc71fcb91bf288cac77296
clouderMark <charnysheuski@yandex.by>
2023-04-21 12:27:59 +0300

    refactor: add posibility set column width

--- store/client/src/components/ContainerWithTwoColumns/styles/containerWithTwoColumns.ts
+++ store/client/src/components/ContainerWithTwoColumns/styles/containerWithTwoColumns.ts
@@ -1,4 +1,4 @@
-const queryMobile = 732;
+export const queryMobile = 732;
 const color = '#6f6f6f';
 
 export const containerWithTwoColumns = {
@@ -11,23 +11,6 @@ export const containerWithTwoColumns = {
 
     [`@media (max-width: ${queryMobile}px)`]: {
       flexDirection: 'column',
-    },
-  },
-
-  column2: {
-    width: '51%',
-
-    [`@media (max-width: ${queryMobile}px)`]: {
-      width: '100%',
-    },
-  },
-
-  column1: {
-    width: '40%',
-
-    [`@media (max-width: ${queryMobile}px)`]: {
-      width: '100%',
-      marginBottom: '16px',
     },
   },
 };
