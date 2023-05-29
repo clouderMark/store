446b98e54bc42ffe54babcea964c4f4cbf4bcbf7
clouderMark <charnysheuski@yandex.by>
2023-05-29 17:20:57 +0300

    fix: prop type and action for image if it unexist

--- store/client/src/components/CentererImage/CentererImage.tsx
+++ store/client/src/components/CentererImage/CentererImage.tsx
@@ -1,23 +1,28 @@
 import {Box} from '@mui/material';
 
 interface IProps {
-  // eslint-disable-next-line
-  img: any;
+  img: string;
 }
 
-const CentererImage = (props: IProps) => (
-  <Box sx={{display: 'flex', justifyContent: 'center'}}>
-    <Box
-      component="img"
-      src={props.img}
-      sx={{
-        width: '100%',
-        maxWidth: '1920px',
-        objectFit: 'cover',
-        maxHeight: '560px',
-      }}
-    />
-  </Box>
-);
+const CentererImage = (props: IProps) => {
+  const {img} = props;
+
+  return (
+    <Box sx={{display: 'flex', justifyContent: 'center'}}>
+      {img !== process.env.REACT_APP_IMG_URL ? (
+        <Box
+          component="img"
+          src={img}
+          sx={{
+            width: '100%',
+            maxWidth: '1920px',
+            objectFit: 'cover',
+            maxHeight: '560px',
+          }}
+        />
+      ) : null}
+    </Box>
+  );
+};
 
 export default CentererImage;
