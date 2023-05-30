2f98c09f3eafa1df506140381f3f3763b7e57e86
clouderMark <charnysheuski@yandex.by>
2023-05-30 15:33:56 +0300

    fix: if no data return null from info component

--- store/client/src/components/Info.tsx
+++ store/client/src/components/Info.tsx
@@ -13,29 +13,33 @@ const Info = (props: IProps) => {
   const {image, header, title, listTitle, listItems, paragraphs} = props.item;
 
   return (
-    <ContainerWithTwoColumns
-      firstColumn={
-        <>{image ? <Box component="img" src={image} sx={{width: '100%', mt: '30px', mb: '30px'}} /> : null}</>
-      }
-      secondColumn={
-        <>
-          {title || header ? (
-            <Box sx={{'& div': {pt: 0, pb: '10px'}}}>
-              <StrongWithTitle content={{p: title, title: header}} titleComponent={'h2'} />
-            </Box>
-          ) : null}
-          {listTitle || listItems.length ? (
-            <ListWithSubheader subheader={listTitle} items={listItems.map((el) => el.value)} />
-          ) : null}
-          {paragraphs.map((el) => (
-            <Typography component="p" key={el.id} sx={{mb: '20px'}}>
-              {el.value}
-            </Typography>
-          ))}
-        </>
-      }
-      buttons={props.buttons}
-    />
+    <>
+      {image || header || title || listTitle || listItems.length || paragraphs.length ? (
+        <ContainerWithTwoColumns
+          firstColumn={
+            <>{image ? <Box component="img" src={image} sx={{width: '100%', mt: '30px', mb: '30px'}} /> : null}</>
+          }
+          secondColumn={
+            <>
+              {title || header ? (
+                <Box sx={{'& div': {pt: 0, pb: '10px'}}}>
+                  <StrongWithTitle content={{p: title, title: header}} titleComponent={'h2'} />
+                </Box>
+              ) : null}
+              {listTitle || listItems.length ? (
+                <ListWithSubheader subheader={listTitle} items={listItems.map((el) => el.value)} />
+              ) : null}
+              {paragraphs.map((el) => (
+                <Typography component="p" key={el.id} sx={{mb: '20px'}}>
+                  {el.value}
+                </Typography>
+              ))}
+            </>
+          }
+          buttons={props.buttons}
+        />
+      ) : null }
+    </>
   );
 };
 
